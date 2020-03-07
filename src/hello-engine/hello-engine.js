c78e86c34b15bf595ebccf848da509138cbe796b
kampamaru <stav.orfanoudakis@gmail.com>
2020-03-07 10:28:13 +0100

    gr-comment-engine
    engine add support workshop's comments

--- src/hello-engine/hello-engine.js
+++ src/hello-engine/hello-engine.js
@@ -3,23 +3,23 @@ const WebSocket = require('ws');
 const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/3.2.json');
 
-(async () => {
-  try {
-    console.log('Creating and opening session.');
-    const session = enigma.create({
-      schema,
-      url: 'ws://localhost:19076/app',
-      createSocket: (url) => new WebSocket(url),
-    });
-    const global = await session.open();
+(async() => {
+    try {
+        console.log('Creating and opening session.');
+        const session = enigma.create({
+            schema,
+            url: 'ws://localhost:19076/app',
+            createSocket: (url) => new WebSocket(url),
+        });
+        const global = await session.open();
 
-    const version = await global.engineVersion();
-    console.log(`Engine version retrieved: ${version.qComponentVersion}`);
-
-    await session.close();
-    console.log('Session closed.');
-  } catch (err) {
-    console.log('Whoops! An error occurred.', err);
-    process.exit(1);
-  }
+        const version = await global.engineVersion();
+        console.log(`Engine version retrieved: ${version.qComponentVersion}`);
+        console.log('Epitelous gamw to mouni tis mana sou!!!')
+        await session.close();
+        console.log('Session closed.');
+    } catch (err) {
+        console.log('Whoops! An error occurred.', err);
+        process.exit(1);
+    }
 })();
